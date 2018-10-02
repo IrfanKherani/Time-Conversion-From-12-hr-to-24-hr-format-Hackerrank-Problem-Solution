f4be4ba8efb67070d62177977de596f8618a7232
IrfanKherani <43785100+IrfanKherani@users.noreply.github.com>
2018-10-02 19:34:35 +0530

    Create timeconverion.py

---
+++ timeconverion.py
@@ -0,0 +1,16 @@
+#
+#Copy the code from below and below the function defination
+#
+
+def timeConversion(s):
+    s1=s;
+    if(s[8:]=="AM" and s[0:2]=="12"):
+        s1="00"+s[2:8];
+        return(s1);
+    elif(s[8:]=="AM"):
+        return(s[:8]);
+    elif(s[8:]=="PM" and s[0:2]=="12"):
+        return(s[:8]);
+    else:
+        s1=str(int(s[:2])+12)+s[2:8];
+        return(s1);
